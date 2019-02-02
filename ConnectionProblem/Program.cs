25b24bcb181593e4edd465bc8ec578482537852d
hemang.vyas <hemang.vyas@britehouse.co.za>
2019-02-02 12:34:07 +0200

    Add project files.

---
+++ ConnectionProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConnectionProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
